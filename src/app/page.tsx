da5f455b9607dfa3597cfcb8673e828a9fa3d802
parikshitRoy <parikshitroy00@gmail.com>
2024-02-16 13:17:01 +0530

    Changed layout.tsx CSS, Created MaxWidthWrapper.tsx component & used it in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <h1>Hippo</h1>;
+  return (
+    <MaxWidthWrapper>
+      <h1>Hippo</h1>
+    </MaxWidthWrapper>
+  );
 }
