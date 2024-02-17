2c145e058a4a65df0ed813ac288b5ad81ab504ec
parikshitRoy <parikshitroy00@gmail.com>
2024-02-17 17:49:48 +0530

    Created `NavItem.tsx` component & rendering it in `NavItems.tsx` passing value to check which NavItem is open. - `NavItems.tsx` maps through `PRODUCT_CATEGORIES` and pass it as category to `NavItem.tsx`.

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,38 @@
 "use client";
 
+import { PRODUCT_CATEGORIES } from "@/config";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
   const [activeIndex, setActiveIdex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
 
-  return <div className="flex gap-4 h-full">NavItem component</div>;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIdex(null);
+          } else {
+            setActiveIdex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
