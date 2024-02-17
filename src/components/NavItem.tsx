b6146dff1e334029b7bbbf68c7872502eb13407e
parikshitRoy <parikshitroy00@gmail.com>
2024-02-17 20:10:54 +0530

    NavItem.tsx CSS change

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,6 +4,8 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = (typeof PRODUCT_CATEGORIES)[number];
 
@@ -16,19 +18,21 @@ interface NavItemProps {
 
 const NavItem = ({ category, handleOpen, isOpen, isAnyOpen }: NavItemProps) => {
   return (
-    <div className="relative flex items-center">
-      <Button
-        className="gap-1.5"
-        onClick={handleOpen}
-        variant={isOpen ? "secondary" : "ghost"}
-      >
-        {category.label}
-        <ChevronDown
-          className={cn("h-4 w-4 transition-all text-muted-foreground", {
-            "-rotate-180": isOpen,
-          })}
-        />
-      </Button>
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground", {
+              "-rotate-180": isOpen,
+            })}
+          />
+        </Button>
+      </div>
     </div>
   );
 };
