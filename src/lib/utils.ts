6558eeca077c83009974e95cf93a0abd14eabbbb
parikshitRoy <parikshitroy00@gmail.com>
2024-02-18 21:37:57 +0530

    Added conditional rendering for `Cart.tsx` Component for Empty & Non-empty Cart and formatted the price as a currency string.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+//! Formatting the price as a currency string according to the specified options.
+export function formatePrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "USD", notation = "compact" } = options;
+  const numaricPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency, // currency, OR currency: currency, both are same
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numaricPrice);
 }
