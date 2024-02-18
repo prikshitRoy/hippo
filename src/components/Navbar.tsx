db137740842fc1b09ad3f81814de13c74424a3de
parikshitRoy <parikshitroy00@gmail.com>
2024-02-18 13:24:17 +0530

    Added conditional rendering for Sign In, Create Account, Cart. Created `Cart.tsx` Component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
-        <MaxWidthWrapper>
+        <MaxWidthWrapper className="max-w-full">
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
               {/* TODO: Add Mobile nav */}
@@ -20,6 +24,51 @@ const Navbar = () => {
               {/* NavItems */}
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+
+              {/* Sign In, Create Account, Cart */}
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign In
+                    </Link>
+                  )}
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p>My acount</p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create Account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
