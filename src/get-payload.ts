af00eff239784fb0a8cdfd026956b8efa4505078
parikshitRoy <parikshitroy00@gmail.com>
2024-02-20 21:50:40 +0530

    `Payload Part 2`: created .env, get-payload.ts, payload.config.ts, next-utils.ts
    .env
    - create `.env` file in root.
    - add `PAYlOAD_SECRET=` , `NEXT_PUBLC_SERVER_URL=http://localhost:3000` , create MongoDB Project & add `MONGODB_URL=`
    
    get-payload.ts
    - Implemented Payload CMS client with caching.
    
    payload.config.ts
    - Configured Payload CMS with MongoDB, Slate editor, and rate limiting
    - Integrated Payload CMS with server URL, admin routes, and meta configuration
    - Set up Payload CMS with TypeScript type generation
    
    next-utils.ts
    - Initialized dynamic port configuration

---
+++ src/get-payload.ts
@@ -0,0 +1,50 @@
+//src/get-payload.ts
+import type { InitOptions } from "payload/config";
+import payload from "payload";
+import dotenv from "dotenv";
+import path from "path";
+
+//! This line of code ensures that the .env file is loaded and its variables are accessible within the Node.js environment.
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+//! Using Caching
+let cached = (global as any).payload;
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYlOAD_SECRET) {
+    throw new Error("PAYlOAD_SECRET is missing");
+  }
+
+  if (cached.client) {
+    return cached.client;
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYlOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (error: unknown) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.client;
+};
