a5063c90b87b8406df6086f7a73f4f59bc51e745
parikshitRoy <parikshitroy00@gmail.com>
2024-02-29 21:59:40 +0530

    Verifying User by sending Email for Verification
    Added Error handling on Sign-up page.
    Added email verification code in `auth-router.ts`.
    Created  `VerifyEmail.tsx` component.It acts as a mediator to pass `token` from browser to payload for email verification.

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Link from "next/link";
+import { toast } from "sonner";
+import { ZodError } from "zod";
 import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { useForm } from "react-hook-form";
 import { Icons } from "@/components/Icons";
+import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ArrowRightIcon } from "lucide-react";
@@ -24,7 +27,29 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+  const router = useRouter();
+
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) => {
+      if (err.data?.code === "CONFLICT") {
+        toast.error("This email is already in use. Sign in instead?");
+
+        return;
+      }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+
+        return;
+      }
+
+      toast.error("Something went wrong. Please try again.");
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}.`);
+      router.push("/verify-email?to=" + sentToEmail);
+    },
+  });
 
   const Submit = ({ email, password }: TAuthCredentialsValidator) => {
     // sending data to server
@@ -63,7 +88,13 @@ const Page = () => {
                       })}
                       placeholder="you@example.com"
                     />
+                    {errors?.email && (
+                      <p className="text-sm text-red-500">
+                        {errors.email.message}
+                      </p>
+                    )}
                   </div>
+
                   {/* Password Field */}
                   <div className="grid gap-1 py-1">
                     <Label htmlFor="Password">Password</Label>
@@ -75,6 +106,11 @@ const Page = () => {
                       })}
                       placeholder="Password"
                     />
+                    {errors?.password && (
+                      <p className="text-sm text-red-500">
+                        {errors.password.message}
+                      </p>
+                    )}
                   </div>
 
                   <Button>Sign up</Button>
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -4,4 +4,4 @@ const t = initTRPC.context().create();
 export const router = t.router;
 
 //! Any one will be able to call this API-end point, its a public end point
-export const publishProcedure = t.procedure;
+export const publicProcedure = t.procedure;
