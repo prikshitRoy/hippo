976055854e5d7d7349260b5eae4c60bc85770817
parikshitRoy <parikshitroy00@gmail.com>
2024-03-01 13:17:46 +0530

    Added Sign-In functionality

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,7 @@
+import { ExpressContext } from "../server";
 import { initTRPC } from "@trpc/server";
 
-const t = initTRPC.context().create();
+const t = initTRPC.context<ExpressContext>().create();
 export const router = t.router;
 
 //! Any one will be able to call this API-end point, its a public end point
