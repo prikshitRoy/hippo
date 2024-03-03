f2b67e0319614331faae118190237e8236141cdb
parikshitRoy <parikshitroy00@gmail.com>
2024-03-03 11:15:59 +0530

    Creating Payload collection Product.ts, Media.ts, Orders.ts, ProductFile.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,8 +1,12 @@
 //src/payload.config.ts
 import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { Products } from "./collections/Products/Products";
+import { ProductFiles } from "./collections/ProductFile";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
+import { Orders } from "./collections/Orders";
 import { buildConfig } from "payload/config";
+import { Media } from "./collections/Media";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
 import path from "path";
@@ -13,7 +17,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, Orders, ProductFiles],
   routes: {
     admin: "/sell",
   },
