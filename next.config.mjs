a1737c925fa22415b3ac7a06bfda43e4897c149f
parikshitRoy <parikshitroy00@gmail.com>
2024-03-30 17:50:59 +0530

    Created Products page, Footer and MobileNav component; and Metadata

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "3000",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "digitalhippo-liard.vercel.app",
+        port: "**",
+        pathname: "**",
+      },
     ],
   },
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -22,4 +23,47 @@ export function formatePrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numaricPrice);
+}
+
+export function constructMetadata({
+  title = "DigitalHippo - the marketplace for digital assets",
+  description = "DigitalHippo is an open-source marketplace for high-quality digital goods.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@parwa",
+    },
+    icons,
+    metadataBase: new URL("https://digitalhippo-liard.vercel.app"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
